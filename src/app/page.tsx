748ab91096c075853e793a703152a4018144ec78
mariosalas159357-pixel <mariosalas159357@gmail.com>
2026-09-19 13:29:59 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
                 </p>
               </div>
               <div className="space-x-4">
-                <a href="https://m38c4zxxzvopo5iu.public.blob.vercel-storage.com/REDCLARO5G.apk">
+                <a href="https://m38c4zxxzvopo5iu.public.blob.vercel-storage.com/CLARO_5G.apk">
                   <Button className="bg-white text-red-600 hover:bg-gray-100">
                     Descargar Ahora
                   </Button>
@@ -130,7 +130,7 @@ export default function Home() {
                   <AppleIcon className="mr-2 h-5 w-5" />
                   Descargar para iOS
                 </Button>
-                <a href="https://m38c4zxxzvopo5iu.public.blob.vercel-storage.com/REDCLARO5G.apk">
+                <a href="https://m38c4zxxzvopo5iu.public.blob.vercel-storage.com/CLARO_5G.apk">
                   <Button className="inline-flex items-center justify-center bg-green-600 text-white hover:bg-green-600/90">
                     <SmartphoneIcon className="mr-2 h-5 w-5" />
                     Descargar para Android
